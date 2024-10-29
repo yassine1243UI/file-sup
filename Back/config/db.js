d53dd03cbb412fbfeb7dcfd861799c51fb216ee6
Sarah Faye <118432192+fayesarah555@users.noreply.github.com>
2024-10-29 20:41:26 +0100

    docker files

--- Back/config/db.js
+++ Back/config/db.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2/promise');
-require('dotenv').config();
+require('dotenv').config({ path: './Back/.env' });
+
 
 const db = mysql.createPool({
     host: process.env.DB_HOST,
